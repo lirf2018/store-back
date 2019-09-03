71c07939f632430cc8581653a48f48957fcc3f4b
lirf <512164882@qq.com>
2019-09-03 14:45:36 +0800

    代码更新

--- src/main/java/com/yufan/controller/XiancaiGoodsController.java
+++ src/main/java/com/yufan/controller/XiancaiGoodsController.java
@@ -73,7 +73,7 @@ public class XiancaiGoodsController {
                 map.put("goods_id", Integer.parseInt(listData.get(i).get("id").toString()));
                 map.put("goods_name", listData.get(i).get("goods_name"));
                 map.put("goods_img", Constants.IMG_URL + listData.get(i).get("goods_img"));
-                map.put("now_price", new BigDecimal(listData.get(i).get("now_price").toString()).setScale(2, BigDecimal.ROUND_HALF_UP));
+                map.put("now_price", listData.get(i).get("now_price").toString());
                 map.put("read_num", Integer.parseInt(listData.get(i).get("read_num").toString()));
                 outList.add(map);
             }
